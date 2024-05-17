bcfa814ddb6fc58987fd949f21ce3d3a22188c92
서민재 <hey_minj@naver.com>
2024-05-18 01:53:19 +0900

    데이터베이스 연동 및 크롤링, 팔로잉 링크

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -1,13 +1,39 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import pymysql
 
 class QuotetutorialPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='@Dmsdnjf12#',
+            database='myquotes',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+        self.curr.execute("""CREATE TABLE quotes_tb (
+                title VARCHAR(255),
+                author VARCHAR(255),
+                tag VARCHAR(255)
+                )""")
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.curr.execute("""INSERT INTO quotes_tb (title, author, tag) VALUES (%s, %s, %s)""", (
+            item['title'][0],
+            item['author'][0],
+            item['tag'][0]
+        ))
+        self.conn.commit()
--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -3,8 +3,10 @@ from ..items import QuotetutorialItem
 class QuoteSpider(scrapy.Spider):
     name = 'quotes' #이름 변수, 스파이더 내부에서 사용할
     start_urls = [ #url 리스트가 필요함
-        'https://quotes.toscrape.com/' #스크랩할 url
+        # 'https://quotes.toscrape.com/' #스크랩할 url
+        'https://quotes.toscrape.com/page/1/'
     ]
+    page_number = 2 #페이지 넘버를 저장할 변수
 
     def parse(self, response):
         items = QuotetutorialItem()
@@ -22,6 +24,17 @@ class QuoteSpider(scrapy.Spider):
             items['tag'] = tag
 
             yield items
+
+        # next_page = response.css('li.next a::attr(href)').get()
+        next_page = f'https://quotes.toscrape.com/page/{str(QuoteSpider.page_number)}/'
+
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self.parse)
+
+        if QuoteSpider.page_number < 11:
+            QuoteSpider.page_number+=1
+            yield response.follow(next_page, callback=self.parse)
+
 
         
 
